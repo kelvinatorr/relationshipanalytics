f3bf7e04a0914eca3f389f66035f559039b9d79f
Kelvin Dealca <kelvin.dealca@gmail.com>
2014-08-15 08:56:24 -0700

    Move hitlist_api_messages to different module

--- hitlist_api.py
+++ hitlist_api.py
@@ -6,6 +6,10 @@ from protorpc import remote
 from time import sleep
 
 import ra_memcache
+# Import api messages
+from hitlist_api_messages import EateryNotes
+from hitlist_api_messages import EateryLocation
+from hitlist_api_messages import EateryLocationCollection
 
 from models import Eatery
 from models import Couple
@@ -16,40 +20,6 @@ IOS_CLIENT_ID = 'replace this with your iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
 package = 'hitlist'
 
-class EateryMessage(messages.Message):
-    restaurant_name = messages.StringField(1)
-    cuisine_type = messages.StringField(2)
-    city = messages.StringField(3)
-    state = messages.StringField(4)
-    notes_comments = messages.StringField(5)
-    completed = messages.BooleanField(6)
-    first_trip_date = message_types.DateTimeField(7)
-    last_visit_date = message_types.DateTimeField(8)
-    number_of_trips = messages.IntegerField(9)
-    days_since_last_trip = messages.IntegerField(10)
-    p1_rating = messages.FloatField(11)
-    p2_rating = messages.FloatField(12)
-    average_rating = messages.FloatField(13)
-    yelp_business_id = messages.StringField(14)
-    street_address = messages.StringField(15)
-    zip_code = messages.IntegerField(16)    
-
-class EateryNotes(messages.Message):
-    restaurant_name = messages.StringField(1)
-    notes_comments = messages.StringField(2)
-    status_code = messages.IntegerField(3)
-
-class EateryLocation(messages.Message):
-    eatery_id = messages.IntegerField(1,variant=messages.Variant.INT64)
-    restaurant_name = messages.StringField(2)    
-    latitude = messages.FloatField(4)
-    longitude = messages.FloatField(5)
-    geocoded = messages.BooleanField(6)
-    status_code = messages.IntegerField(7)
-
-class EateryLocationCollection(messages.Message):
-    """Collection of EateryLocations."""
-    items = messages.MessageField(EateryLocation, 1, repeated=True)
 
 @endpoints.api(name='hitlist',version='v1'
                 ,allowed_client_ids=[WEB_CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID]
