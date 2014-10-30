66f1c2a87c19142f6d867f145cf82cf9ee12b306
Kelvin Dealca <kelvin.dealca@gmail.com>
2014-10-29 23:37:00 -0700

    add hitlist mapping to ra.hitlist js object

--- js/hitlist-mapping/hitlist-mapping-main.js
+++ js/hitlist-mapping/hitlist-mapping-main.js
@@ -1,3 +1,18 @@
-(function(){
+/**
+ * @fileoverview
+ * Provides methods for the mapping feature of the hitlist project.
+ */
+
+/** global namespace for relationshipanalytics projects. */
+var ra = ra || {};
+
+/** hitlist namespace for hitlist project. */
+ra.hitlist = ra.hitlist || {};
+
+/** hitlist namespace for hitlist editing. */
+ra.hitlist.mapping = ra.hitlist.mapping || {};
+
+ra.hitlist.mapping.loadTest = function(){
 	console.log("Helloooo");
-})();
+};
+	
