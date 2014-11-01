9edecc7f926c18e445f3be1ba3d5f0f9a2356ad4
Kelvin Dealca <kelvin.dealca@gmail.com>
2014-10-31 23:33:06 -0700

    Get closest points and add them to the map

--- js/hitlist-mapping/hitlist-mapping-main.js
+++ js/hitlist-mapping/hitlist-mapping-main.js
@@ -61,10 +61,69 @@ ra.hitlist.map.geoLocateUser = function(){
   }
 };
 
+/**
+ * Calculate the distance in kilometers between 2 geographic points.
+ * @param {number} Latitude of point1
+ * @param {number} Longitude of point1
+ * @param {number} Latitude of point2
+ * @param {number} Longitude of point2
+ * @return {number} Distance in km between the 2 coordinates.
+ */
+ra.hitlist.map.calcDistance = function(lat1, lon1, lat2, lon2) {
+    var R = 6371;
+    var a =
+        0.5 - Math.cos((lat2 - lat1) * Math.PI / 180) / 2 +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        (1 - Math.cos((lon2 - lon1) * Math.PI / 180)) / 2;
+    return R * 2 * Math.asin(Math.sqrt(a));
+};
+
+/**
+ * Find the 10 closest points from a users location.
+ * @param {Array.object} List of hitlist locations
+ * @return {Array.object} List of 10 closest points
+ */
+ra.hitlist.map.findClosestPoints = function(hitlistLocations) {
+    for (var i = 0; i < hitlistLocations.length; i++) {
+        var e = hitlistLocations[i];
+        var d2 = ra.hitlist.map.calcDistance(ra.hitlist.map.CURRENT_LOCATION.latitude, ra.hitlist.map.CURRENT_LOCATION.longitude, e.latitude, e.longitude);
+        e.distance = d2;
+        //alert(d2);
+    }
+    // Sort result list ascending.
+    hitlistLocations.sort(function(a, b) {
+        return a.distance - b.distance;
+    });
+    // Return the top 10 closest.
+    return hitlistLocations.slice(0, 10);
+};
+
+/**
+ * Add hitlist location markers to the google-map element
+ */
+ra.hitlist.map.addMapMarkers = function() {
+    // Run api calling function.    
+    gapi.client.hitlist.eateries.getLocations().execute(function(resp) {
+    	var hitlistLocations = resp.items;        
+        // Filter for the 10 closest points.
+        var tenClosest = ra.hitlist.map.findClosestPoints(hitlistLocations);        
+        // Add the 10 closest to the map as markers
+        for (var i = 0; i < tenClosest.length;i++){
+            var n = document.createElement("google-map-marker");
+            n.setAttribute('latitude',tenClosest[i].latitude);
+            n.setAttribute('longitude',tenClosest[i].longitude);
+            // Set the title of the marker
+            n.setAttribute('title', 'Hi Kelvin');
+            // Set a h3 tag as a child of the marker so it shows up when they click on it.
+            document.querySelector('google-map').appendChild(n);
+        }
+    });    
+};
+
 ra.hitlist.map.initialize = function(){
   geocoder = new google.maps.Geocoder();
   // Get current location.
-  ra.hitlist.map.geoLocateUser();
-  //ra.hitlist.map.addMapMarkers();
+  ra.hitlist.map.geoLocateUser();  
+  ra.hitlist.map.addMapMarkers();
 };
 	
