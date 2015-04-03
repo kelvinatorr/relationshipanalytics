d486d336b353e79cc477beed3264b75928231847
Kelvin Dealca <kelvin.dealca@gmail.com>
2015-04-02 20:09:12 -0700

    Copy starter app from ground up

--- js/app/app.js
+++ js/app/app.js
@@ -2,10 +2,27 @@
  * Created by kdealca on 3/31/2015.
  */
 (function(){
-    var app = angular.module('raapp', ['ngMaterial']);
+    var app = angular.module('raApp', ['ngMaterial']);
 
-    app.run(function($log){
-        $log.debug("starterApp + ngMaterial running...");
-    });
+    app.config(['$mdThemingProvider', '$mdIconProvider', function($mdThemingProvider, $mdIconProvider){
+
+        $mdIconProvider
+            .defaultIconSet("static/svg/avatars.svg", 128)
+            .icon("menu"       , "static/svg/menu.svg"        , 24);
+            //.icon("share"      , "./assets/svg/share.svg"       , 24)
+            //.icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+            //.icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+            //.icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+            //.icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+
+    }]);
+
+    app.run(['$log',function($log){
+        $log.debug("starterApp + ngMaterial running kelvin...");
+    }]);
 
 })();
